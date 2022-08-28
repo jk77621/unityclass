8285dcb4fd3bee2adeaa2802fa183cad245f324f
jk77621 <67351218+jk77621@users.noreply.github.com>
2022-08-28 10:56:40 +0900

    Class01
    2022-08-28 Class01 Test

---
+++ My project/Assets/Scripts/testScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("K");
+        }
+    }
+}
