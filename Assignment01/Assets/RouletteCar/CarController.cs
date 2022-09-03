94081e9848a00904d80482c7c4b5e55eb10912fe
jk77621 <jk77621@naver.com>
2022-09-03 09:04:06 +0900

    Assignment01
    2022-09-03 Logic Correction

--- Assignment01/Assets/RouletteCar/CarController.cs
+++ Assignment01/Assets/RouletteCar/CarController.cs
@@ -6,16 +6,19 @@ using UnityEngine.UI;
 public class CarController : MonoBehaviour
 {
     GameObject objCar;
+    GameObject objFlag;
     GameObject objDistance;
     float nCarSpeed = 0; //자동차 속도
     Vector2 vecStart;
     Vector2 vecEnd;
     int nTry = 0; //시도 횟수
+    bool isPlay = false;
 
     // Start is called before the first frame update
     void Start()
     {
         objCar = GameObject.Find("car");
+        objFlag = GameObject.Find("flag");
         objDistance = GameObject.Find("Distance");
     }
 
@@ -24,13 +27,22 @@ public class CarController : MonoBehaviour
     {
         nTry = int.Parse(objDistance.GetComponent<Text>().text.Substring(objDistance.GetComponent<Text>().text.Length - 1, 1));
 
-        if (nTry == 0) return;
+        if (nTry == 0)
+        {
+            if (!isPlay) return;
+
+            objDistance.GetComponent<Text>().text = objCar.transform.position.x > objFlag.transform.position.x - 3 && objCar.transform.position.x < objFlag.transform.position.x + 3 ? "승리" : "실패";
+
+            isPlay = false;
+            return;
+        }
 
         //마우스 클릭했을 때
         if (Input.GetMouseButtonDown(0))
         {
             //마우스의 위치좌표 저장
             vecStart = Input.mousePosition;
+            isPlay = true;
         }
 
         //마우스 클릭에서 땠을 때
